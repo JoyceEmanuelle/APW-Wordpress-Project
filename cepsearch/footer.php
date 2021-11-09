9e001c3bc0bf4c4295ecda2e1292aa7ff62bd62a
JoyceEmanuelle <51747364+JoyceEmanuelle@users.noreply.github.com>
2021-11-08 22:58:03 -0300

    Add files via upload

---
+++ cepsearch/footer.php
@@ -0,0 +1,22 @@
+	<div class="credits section bg-dark no-padding">
+	
+		<div class="credits-inner section-inner group">
+	
+			<p class="credits-left">
+				&copy; <?php echo date( 'Y' ); ?> <a href="<?php echo esc_url( home_url() ); ?>"><?php bloginfo( 'name' ); ?></a>
+			</p>
+			
+			<p class="credits-right">
+				<span>Theme by Eduardo Laffot and Joyce Emanuelle Pinheiro Piva 3MIN</span>
+			</p>
+					
+		</div><!-- .credits-inner -->
+		
+	</div><!-- .credits -->
+
+</div><!-- .big-wrapper -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
